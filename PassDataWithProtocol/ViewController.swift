9de806335bef0ac063641b9acaceaacfb7e4c4b7
ibrahim doğan <ibrahimdogan@Murats-MacBook-Pro.local>
2019-03-14 11:08:02 +0300

    Initial Commit

---
+++ PassDataWithProtocol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassDataWithProtocol
+//
+//  Created by ibrahim doğan on 14.03.2019.
+//  Copyright © 2019 ibrahim doğan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
