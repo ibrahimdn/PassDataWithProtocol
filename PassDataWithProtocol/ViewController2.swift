ca7dbd958a683e85e76126620e156e7a764471e9
ibrahim doğan <ibrahimdogan@Murats-MacBook-Pro.local>
2019-03-14 11:23:34 +0300

    first commit

---
+++ PassDataWithProtocol/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  PassDataWithProtocol
+//
+//  Created by ibrahim doğan on 14.03.2019.
+//  Copyright © 2019 ibrahim doğan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
